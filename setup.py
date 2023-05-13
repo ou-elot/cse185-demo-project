ccf76b3afbd10cb35c182ca8eef38430eeb0c643
gymreklab <melissa.gymrek@gmail.com>
2023-05-12 21:16:12 -0700

    adding setup.py script

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+  
+setup(
+    name='mypileup',
+    version='0.0',
+    description='CSE185 Demo Project',
+    author='Melissa Gymrek',
+    author_email='mgymrek@ucsd.edu',
+    packages=["src"],
+    entry_points={
+        "console_scripts": [
+            "mypileup=src.mypileup:main"
+        ],
+    },
+    install_requires=[],
+)
